a6a31a4f7f08efeb910b0ca83abad971247d9e03
SerfLuizDavi <daviluizds10@gmail.com>
2024-11-23 08:46:56 -0300

    Add interação

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-/* 51:23 */
+const modal = document.querySelector(".modal")
+const mascaramodal = document.querySelector(".mascara_modal")
+function mostrarmodal(){
+    modal.style.left = "50%"
+    mascaramodal.style.visibility = "visible"
+}
+
+function escondermodal(){
+    modal.style.left = "-30%"
+    mascaramodal.style.visibility = "hidden"
+}
